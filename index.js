cecc2ac2a2fcf98405e6b154639c3ed87bc55e83
Dave Yu <yuichiuyu1915@gmail.com>
2022-08-01 15:55:35 +0800

    Added export functionality

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const imageContainer = document.querySelector('.image-container');
+const userImage = document.getElementById('user_image');
 
-// handle image upload
+/* Functions */
+
 function handleImageUpload(event) {
   event.preventDefault();
   const [file] = document.getElementById('file').files;
-  document.getElementById('user_image').src = URL.createObjectURL(file)
+  userImage.src = URL.createObjectURL(file);
+  // userImage.onload = () => exportImage();
+  userImage.onload = () => document.getElementById('download').style.display = 'block';
 }
 
-document.getElementById('upload').addEventListener('submit', handleImageUpload)
+function exportImage() {
+  html2canvas(document.querySelector(".image-container")).then(canvas => {
+    const out = document.getElementById('out');
+    
+    out.innerHTML = '';
+    out.appendChild(canvas)
+    var image = canvas.toDataURL();
+
+    var link = document.createElement("a");
+    link.setAttribute('download', 'myfile.jpg');
+    link.href = image;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  });
+}
+
+/* --------------------------- */
+
+/* Event Listeners  */
 
 // adjust border color
 document.getElementById('white_border').addEventListener('change', () => {
@@ -19,6 +42,15 @@ document.getElementById('3x3').addEventListener('change', () => {
   imageContainer.classList.toggle('grid9');
 })
 
-// Initial actions
+document.getElementById('upload').addEventListener('submit', handleImageUpload)
+
+document.getElementById('download').addEventListener('click', exportImage)
+
+/* --------------------------- */
+
+/* Initial actions */
+
 document.getElementById('white_border').checked = false;
 document.getElementById('3x3').checked = false;
+
+/* --------------------------- */
